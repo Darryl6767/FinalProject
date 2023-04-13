9a57e771549ece0d1613b31617a43683150659d9
Renzo-Franco <rfrancov@mail.umw.edu>
2023-04-13 12:17:40 -0400

    Started main2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main { 
+import javax.swing.*;
+public class Main extends javax.swing.JFrame{
     public static void main(String[] args) {
         JFrame frame = new JFrame("Converter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
