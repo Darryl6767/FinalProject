d211161a3ce9c8e06392a54c201a5c43083a5503
Renzo-Franco <rfrancov@mail.umw.edu>
2023-04-16 17:16:45 -0400

    Layout, background color, and km to mi button

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 public class Main extends javax.swing.JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Converter");
-        JButton mtoKm = new JButton("Miles to Kilometers");
-        mtoKm.addActionListener(new ActionListener() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Layout
+        frame.setPreferredSize(new Dimension(700,600));
+
+        //Color
+        JPanel panel = new JPanel();
+        panel.setBackground(new java.awt.Color(167,199,231));
+        frame.add(panel);
+
+        //Button km to mi
+        JButton mitoKm = new JButton("Miles to Kilometers");
+        mitoKm.setPreferredSize(new Dimension(200,50));
+        mitoKm.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Nothing here yet");
+                double value = Double.parseDouble(JOptionPane.showInputDialog(frame, "Enter value:"));
+                MileConverter i = new MileConverter();
+                double finalValue = i.convertMtoKm(value);
+                DecimalFormat df = new DecimalFormat("#.#");
+                JOptionPane.showMessageDialog(frame, df.format(finalValue));
             }
         });
-        frame.getContentPane().add(mtoKm);
+        panel.add(mitoKm);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
